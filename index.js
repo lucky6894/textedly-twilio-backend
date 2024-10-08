bfff252c125cc3d9b337b45b209032f2005dca7a
lucky6894 <full.luck@outlook.com>
2024-10-08 07:43:05 -0400

    get phone numbers list

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require("dotenv").config({ path: "./.env" });
 
 const twilio = require("twilio");
+const express = require("express");
+const cors = require("cors");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+const port = process.env.PORT || 8000;
 
-client.incomingPhoneNumbers.list((err, items) => {
-  console.log(items.length);
+const client = twilio(accountSid, authToken);
+const app = express();
+
+app.use(cors());
+app.listen(port, () => console.log(`Server started ${port}`));
+
+app.get("/phone-numbers", (req, res) => {
+  client.incomingPhoneNumbers.list((err, items) => {
+    res.json(items.map(item => item.phoneNumber));
+  });
 });
