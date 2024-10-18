ba84c2de81f198faad190b66ad0c7983a8f0cb45
lucky6894 <full.luck@outlook.com>
2024-10-18 16:16:14 -0400

    finish sending attached file

--- index.js
+++ index.js
@@ -3,27 +3,44 @@ require("dotenv").config({ path: "./.env" });
 const twilio = require("twilio");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const { MessagingResponse } = require('twilio').twiml;
+const { Server } = require("socket.io");
+const multer = require("multer");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const port = process.env.PORT || 8000;
 
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (_, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+})
+const upload = multer({ storage });
 const client = twilio(accountSid, authToken);
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.listen(port, () => console.log(`Server started ${port}`));
+const server = require("http").createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
-app.get("/phone-numbers", (req, res) => {
+app.use(cors());
+app.use("/", express.static("./public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/api/phone-numbers", (req, res) => {
   client.incomingPhoneNumbers.list((err, items) => {
     res.json(items.map(item => item.phoneNumber));
   });
 });
 
-app.get("/statistics", async (req, res) => {
+app.get("/api/statistics", async (req, res) => {
   const result = {};
   try {
     const balance = await client.balance.fetch();
@@ -33,13 +50,37 @@ app.get("/statistics", async (req, res) => {
     };
 
     res.json(result);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-app.get("/messages", (req, res) => {
+app.get("/api/messages", (req, res) => {
   const pageNumber = req.body.pageNumber || 0;
-  client.messages.page({pageSize: 50, pageNumber})
+  client.messages.page({ pageSize: 50, pageNumber })
     .then(data => res.json(data));
 });
+
+app.post("/receive-message", (req, res) => {
+  io.emit("receive-message", JSON.stringify(req.body));
+  // io.emit("receive-message", JSON.stringify(response));
+
+  const twiml = new MessagingResponse();
+  res.type('text/xml').send(twiml.toString());
+});
+
+app.post("/api/send-message", upload.single('file'), async (req, res) => {
+  if (true || req.body.when == "now") {
+    try {
+      await client.messages.create({
+        body: req.body.message,
+        from: req.body.from,
+        to: req.body.to,
+        ...(req.file && { mediaUrl: `http://20.47.120.34/uploads/${req.file?.filename}` })
+      });
+    } catch { }
+  } else { }
+  res.end();
+});
+
+server.listen(port, () => console.log(`Server started ${port}`));
